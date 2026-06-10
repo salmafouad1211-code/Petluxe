4832b746a791a0e5996edc43847c00c98ae21dab
fouadsalma948-maker <ton-email-github>
2026-06-10 21:06:56 +0200

    Hardcode Railway credentials to bypass UI issues

--- backend/config/app.php
+++ backend/config/app.php
@@ -97,7 +97,7 @@ return [
 
     'cipher' => 'AES-256-CBC',
 
-    'key' => env('APP_KEY'),
+    'key' => env('APP_KEY', 'base64:IHtgLomeWdF0UhtoIARjunAU3nVvLJDtCvK6czBI81o='),
 
     'previous_keys' => [
         ...array_filter(
